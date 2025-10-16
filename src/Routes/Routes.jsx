683aefc9858cd73657bdd6309e8333ccae1ff023
himel2535 <monwarhossanhimel@gmail.com>
2025-10-17 02:18:05 +0600

    sign in and sign up page ui is made

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,8 @@ import MainLayout from "../layouts/MainLayout";
 import Home from "../Pages/Home/Home";
 import AboutUs from "../Pages/AboutUs/AboutUs";
 import Profile from "../Pages/Profile/Profile";
+import Signin from "../Pages/Signin";
+import Signup from "../Pages/Signup";
 
 export const router = createBrowserRouter([
   {
@@ -20,6 +22,14 @@ export const router = createBrowserRouter([
       {
         path: "/profile",
         Component: Profile,
+      },
+      {
+        path: "/signin",
+        Component: Signin,
+      },
+      {
+        path: "/signup",
+        Component:Signup,
       },
     ],
   },
